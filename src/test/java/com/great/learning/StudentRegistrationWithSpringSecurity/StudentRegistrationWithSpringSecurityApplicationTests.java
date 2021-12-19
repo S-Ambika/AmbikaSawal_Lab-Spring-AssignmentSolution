f50665928f47cdf1d0c57cc665a9b08101f4ceab
Ambika <ambikasawal91@gmail.com>
2021-12-19 20:31:08 +0530

    solution

---
+++ src/test/java/com/great/learning/StudentRegistrationWithSpringSecurity/StudentRegistrationWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.great.learning.StudentRegistrationWithSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationWithSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
